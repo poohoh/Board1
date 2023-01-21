219f586c493ba10f3676bc291abeba531f8e8226
poohoh <kjh980328@gmail.com>
2023-01-21 22:24:20 +0900

    boardlist

--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardReposiory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -14,5 +16,22 @@ public class BoardService {
     public void write(Board board) {
 
         boardReposiory.save(board);
+    }
+
+    public List<Board> boardList() {
+        return boardReposiory.findAll();
+    }
+
+    public void saveTestData() {
+
+        for (int i = 0; i < 120; i++) {
+            Board board = new Board();
+
+            board.setTitle("제목" + (i + 1));
+            board.setContent("내용" + (i + 1));
+            boardReposiory.save(board);
+
+            //board.setId(null);
+        }
     }
 }
